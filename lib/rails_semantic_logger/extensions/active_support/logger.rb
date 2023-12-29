a5af8f9d07551193b1bf0d5aafeb5ed2cadd1929
Yuri Smirnov <tycooon@yandex.ru>
2023-12-29 17:18:14 +0300

    wip

--- lib/rails_semantic_logger/extensions/active_support/logger.rb
+++ lib/rails_semantic_logger/extensions/active_support/logger.rb
@@ -4,17 +4,12 @@ module ActiveSupport
   # More hacks to try and stop Rails from being it's own worst enemy.
   class Logger
     class << self
-      undef :logger_outputs_to?, :broadcast
+      undef :logger_outputs_to?
     end
 
     # Prevent Console from trying to merge loggers
     def self.logger_outputs_to?(*args)
       true
-    end
-
-    # Prevent broadcasting since SemanticLogger already supports multiple loggers
-    def self.broadcast(logger)
-      Module.new
     end
 
     def self.new(*args, **kwargs)
